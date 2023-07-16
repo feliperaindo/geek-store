937b3ea4881e4b390de7b3517300ec02213e6f99
Felipe <felipe.raindo@gmail.com>
2023-07-16 19:00:16 -0300

    100% cobertura da camada product service

---
+++ tests/mocks/products.ts
@@ -0,0 +1,27 @@
+export const PRODUCT_1 = {
+  id: 1,
+  name: 'Arco Atirador do Sol',
+  price: '50 peças de ouro',
+  orderId: 3
+}
+
+export const PRODUCT_2 = {
+  id: 2,
+  name: 'Lança de Clarear Céu',
+  price: '2 almas de dragões',
+  orderId: 1
+}
+
+export const PRODUCT_2_NO_ID = {
+  name: 'Lança de Clarear Céu',
+  price: '2 almas de dragões',
+  orderId: 1
+}
+
+export const PRODUCT_2_RETURN = {
+  id: 2,
+  name: 'Lança de Clarear Céu',
+  price: '2 almas de dragões',
+}
+
+export const PRODUCT_LIST = [PRODUCT_1, PRODUCT_2]
--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -1,7 +1,38 @@
+// Bibliotecas
 import { expect } from 'chai';
 import sinon from 'sinon';
 
-describe('ProductsService', function () {
+// mocks
+import * as mock from '../../mocks/exporter'
+
+// Model
+import { productModel } from '../../../src/database/models/exporter'
+
+// Camada a ser testada
+import { productService } from '../../../src/service/exporter'
+
+describe('Sequência de testes sobre a camada service responsável pelos produtos', function () {
   beforeEach(function () { sinon.restore(); });
 
+  it('Verifica o retorno da camada service ao requisitar todos os produtos', async function () {
+    const fakeModel = productModel.bulkBuild(mock.products.PRODUCT_LIST);
+
+    const fakeMethod = sinon.stub(productModel, 'findAll').resolves(fakeModel);
+
+    const all = await productService.getAll();
+    
+    sinon.assert.calledOnce(fakeMethod);
+    expect(all).to.be.deep.equal(fakeModel);
+  })
+
+  it('Verifica o retorno da camada service ao cadastrar um novo produto', async function () {
+    const fakeModel = productModel.build(mock.products.PRODUCT_2);
+
+    const fakeMethod = sinon.stub(productModel, 'create').resolves(fakeModel);
+
+    const success = await productService.postProduct(mock.products.PRODUCT_2_NO_ID);
+
+    sinon.assert.calledOnce(fakeMethod)
+    expect(success).to.be.deep.equal(mock.products.PRODUCT_2_RETURN)
+  })
 });
